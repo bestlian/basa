853fcfe0abda3c309e9d5989dde9648d5aef1cee
Dila Januar Bestlian <dila.bestlian@gmail.com>
2023-03-17 16:35:48 +0700

    delete django framework

--- be/basaAPI/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Basa
-
-# Register your models here.
-admin.site.register(Basa)
--- be/basaAPI/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BasaapiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'basaAPI'
